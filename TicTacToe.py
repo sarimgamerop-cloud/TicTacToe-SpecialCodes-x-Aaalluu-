a5bdfa88a9971a2337f33889d9202716d8f7826d
SpecialCodes! <sarimgamerop@gmail.com>
2025-09-24 12:36:00 +0500

    Updated 24 September Now !
    added win system working and restart options ..

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,16 +1,99 @@
 import tkinter as tk
+from tkinter import messagebox as tmsg
+import time
 
 root = tk.Tk()
 root.geometry("451x393")
 root.maxsize(451,393)
+root.title("Tic Tac Toe")
 
 turn = 'X'
+
+def check_win():
+    if b1['text'] == b2['text'] == b3['text'] != "":
+        print("You won!")
+        yesno = tmsg.askquestion(" Wohoo !","You Won ! want Rematch ?")
+        if yesno == 'yes':
+            reset()
+        else:
+            time.sleep(5)
+            root.quit()
+
+    if b4['text'] == b5['text'] == b6['text'] != "":
+        print("You won!")
+        yesno = tmsg.askquestion("Wohoo !","You Won ! want Rematch ?")
+        if yesno == 'yes':
+            reset()
+        else:
+            time.sleep(5)
+            root.quit()
+        
+    if b7['text'] == b8['text'] == b9['text'] != "":
+        print("You won!")
+        yesno = tmsg.askquestion("Wohoo !","You Won ! want Rematch ?")
+        if yesno == 'yes':
+            reset()
+        else:
+            time.sleep(5)
+            root.quit()
+        
+    if b1['text'] == b4['text'] == b7['text'] != "":
+        print("You won!")
+        yesno = tmsg.askquestion("Wohoo !","You Won ! want Rematch ?")
+        if yesno == 'yes':
+            reset()
+        else:
+            time.sleep(5)
+            root.quit()
+        
+    if b2['text'] == b5['text'] == b8['text'] != "":
+        print("You won!")
+        yesno = tmsg.askquestion("Wohoo !","You Won ! want Rematch ?")
+        if yesno == 'yes':
+            reset()
+        else:
+            time.sleep(5)
+            root.quit()
+       
+    if b3['text'] == b6['text'] == b9['text'] != "":
+        print("You won!")
+        yesno = tmsg.askquestion("Wohoo !","You Won ! want Rematch ?")
+        if yesno == 'yes':
+            reset()
+        else:
+            time.sleep(5)
+            root.quit()
+    
+    if b1['text'] == b5['text'] == b9['text'] != "":
+        print("You won!")
+        yesno = tmsg.askquestion("Wohoo !","You Won ! want Rematch ?")
+        if yesno == 'yes':
+            reset()
+        else:
+            time.sleep(5)
+            root.quit()
+
+    if b3['text'] == b5['text'] == b7['text'] != "":
+        print("You won!")
+        yesno = tmsg.askquestion(" Wohoo !","You Won ! want Rematch ?")
+        if yesno == 'yes':
+            reset()
+        else:
+            time.sleep(5)
+            root.quit()
+        
 
 def change(button):
     global turn
     if button['text'] == "":
         button.config(text=turn)
+        check_win()
         turn = 'O' if turn == 'X' else "X"
+
+def reset():
+    for buttons in (b1,b2,b3,b4,b5,b6,b7,b8,b9):
+        buttons.config(text="")
+
 
     
     
